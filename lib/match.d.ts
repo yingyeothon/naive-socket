f141acf4b580734c119042695f09605015a51c22
jaeyoung.choi <lactrious@gmail.com>
2020-01-02 22:14:28 +0900

    Publish v0.2.2

--- lib/match.d.ts
+++ lib/match.d.ts
@@ -1,16 +1,12 @@
-export default class Matcher {
+export default class TextMatch {
     private readonly buffer;
     private captured;
     private pos;
     private error;
     constructor(buffer: string);
-    check(value: string): this;
     capture(endMark: string): this;
-    forward(delta: number): this;
-    loop(capturedIndex: number, loopCallback: (index: number, match: Matcher) => any): this;
-    value(capturedIndex: number, defaultValue?: string): string;
     values(): string[];
     evaluate(): number;
-    private ensureState;
 }
-export declare const withMatcher: (check: (m: Matcher) => Matcher) => (buffer: string) => number;
+export declare type TextMatchChain = (m: TextMatch) => TextMatch;
+export declare const withMatch: (check: TextMatchChain) => (buffer: string) => number;

b7c568647fb27e1346d8e76161ea9aecf885dd89
jaeyoung.choi <lactrious@gmail.com>
2020-06-22 21:54:15 +0900

    v0.2.3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,11 +3,21 @@ interface ILogger {
     warn: typeof console.warn;
     error: typeof console.error;
 }
+declare type ConnectionStateListener = (params: {
+    socket: NaiveSocket;
+    state: ConnectionState;
+}) => void;
 interface INaiveSocketOptions {
     host: string;
     port: number;
     connectionRetryInterval?: number;
     logger?: ILogger;
+    onConnectionStateChanged?: ConnectionStateListener;
+}
+declare enum ConnectionState {
+    Connecting = "Connecting",
+    Connected = "Connected",
+    Disconnected = "Disconnected"
 }
 interface ISendWorkOptions {
     fulfill: ((buffer: string) => number) | RegExp | number;
@@ -20,16 +30,18 @@ export default class NaiveSocket {
     private readonly host;
     private readonly port;
     private readonly logger;
+    private readonly onConnectionStateChanged;
     private readonly sendWorks;
     private currentBuffer;
     private connectionState;
     private socket;
     private alive;
     private connectionRetryInterval;
-    constructor({ host, port, connectionRetryInterval, logger }: INaiveSocketOptions);
+    constructor({ host, port, connectionRetryInterval, logger, onConnectionStateChanged, }: INaiveSocketOptions);
     send: (request: ISendWorkArguments & Partial<ISendWorkOptions>) => Promise<string>;
     disconnect: () => void;
     private buildSendWork;
+    private changeConnectionState;
     private connect;
     private doDisconnect;
     private onConnect;
